85b64bb86bd61be19dc8d236ad75b2251e520594
wangzengdi <sunnyadi@163.com>
2018-09-09 23:12:48 +0800

    optimize backup-init.sh

--- backup-init.sh
+++ backup-init.sh
@@ -7,6 +7,20 @@ HOOK_DIR="$GIT_ROOT_DIR/git-hook"
 hexo_git=(.git .deploy_git)
 
 cd "$HEXO_DIR"
+
+npm i hexo-cli -g
+
+yarn
+
+rm -rf ./themes/next
+git clone https://github.com/theme-next/hexo-theme-next themes/next
+
+# 修复锚点不生效问题
+cp ./init_source/hexo-renderer-markdown-it/lib/renderer.js ./node_modules/hexo-renderer-markdown-it/lib/renderer.js
+
+# 自动配置 favicon
+cp ./init_source/next/_config.yml ./themes/next/_config.yml
+
 for dir_name in ${hexo_git[@]}
 do
     rm -rf "$HEXO_DIR/$dir_name"
